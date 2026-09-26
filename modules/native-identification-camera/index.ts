3fba63336b428ef90e94081d02db574244f4c2cb
Matthew Manning <29759470+matthewdmanning@users.noreply.github.com>
2026-09-25 23:33:52 -0400

    feat(camera): export native camera interface

---
+++ modules/native-identification-camera/index.ts
@@ -0,0 +1,10 @@
+export { NativeIdentificationCameraView } from './src/NativeIdentificationCameraView'
+export type {
+  NativeCapturedPhoto,
+  NativeCameraPosition,
+  NativeCaptureOptions,
+  NativeFlashMode,
+  NativeIdentificationCameraProps,
+  NativeIdentificationCameraRef,
+  NormalizedSubjectRegion,
+} from './src/types'
